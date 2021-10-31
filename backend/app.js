1eead3022caf16bae34e51d1bdf2e81b9bf24c9b
mablds <marcelobragalemos@gmail.com>
2021-10-31 13:29:25 -0300

    chore(remove lib): remove unecessary lib

--- backend/app.js
+++ backend/app.js
@@ -2,24 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = 4000;
 const cors = require('cors');
-const bodyParser=require('body-parser');
 const customerRouter=require('./routes/customer.router');
-
 
 //enable all cors requests
 app.use(cors());
 
 //Bodyparser middleware
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(express.text());
 
 //routes
 app.use('/',customerRouter);
 
-app.use(require('body-parser').urlencoded({extended:false}));
-app.use(require('body-parser').json());
-app.use(require('body-parser').text());
-
-app.listen(PORT, function(){
-    console.log('server is running on port:',PORT);
-});
+app.listen(PORT, () => console.log('server is running on port:',PORT));
